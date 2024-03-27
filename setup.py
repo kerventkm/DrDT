4e6d12594a56f3877763085d932e1749a5d03efe
Kerven Durdymyradov <durdymk@kw-19509.kaust.edu.sa>
2024-03-27 17:53:09 +0300

    sdf

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='drdt',
+    name='DrDT',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'datasets': ['/DecisionRuleSystems/*', 'UCIMLDatasets/*'],
+        '': ['datasets/DecisionRuleSystems/*.csv'],
     },
 )
